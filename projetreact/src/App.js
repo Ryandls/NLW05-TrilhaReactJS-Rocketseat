5e4d104bca375a9c8c7870af59ee495cd5324af8
Bryan Douglas Lima Dos Santos <bryandouglasxd@gmail.com>
2021-03-06 01:32:47 -0300

    First Test

--- projetreact/src/App.js
+++ projetreact/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Number } from './Components/number';
 
 function App() {
   return (  
     <div>
       Tô com fome.
+      <Number/><Number/><Number/><Number/><Number/><Number/>
     </div>
     );
 }
---
+++ projetreact/src/Components/number.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export function Number(){
+    return(
+        <div>
+            number 1
+        </div>
+    )
+}
